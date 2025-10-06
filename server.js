ec054a16328d385ac77a93c0b822108615baadad
Maruti Dalvi <148435084+maruti-dalvi-03@users.noreply.github.com>
2025-10-06 23:19:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://personal-portfolio-fh3v.onrender.com',
+  origin: 'https://personal-portfolio-frotend.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
