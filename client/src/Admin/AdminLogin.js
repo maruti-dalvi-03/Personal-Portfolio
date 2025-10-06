f465cbcdc84c232682ff3736d930f5973e5307b6
Maruti Dalvi <148435084+maruti-dalvi-03@users.noreply.github.com>
2025-10-07 00:20:23 +0530

    fix bug in admin login

--- client/src/Admin/AdminLogin.js
+++ client/src/Admin/AdminLogin.js
@@ -10,7 +10,7 @@ function AdminLogin({ closePopup, onLoginSuccess }) {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5001/api/users/login", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
